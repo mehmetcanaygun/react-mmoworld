bb70a602f3add6ece73a8f7ca622e56bde3fe0b3
Mehmet Can Aygün <mehmetcanaygun@gmail.com>
2022-05-07 16:11:00 +0300

    Create Footer component

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="bg-dark">
+      <div className="container mx-auto px-2 md:px-0 h-40 flex flex-col justify-center items-center">
+        <span className="text-4xl text-light font-macondo">MMOWorld</span>
+        <span className="text-sm text-secondary mt-5">
+          <a
+            href="https://www.github.com/mehmetcanaygun"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mehmet Can Aygün
+          </a>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
