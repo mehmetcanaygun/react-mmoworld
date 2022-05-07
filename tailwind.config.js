1a77988934fcf177e26bb133bcdf6a837dbf4488
Mehmet Can Aygün <mehmetcanaygun@gmail.com>
2022-05-07 12:51:43 +0300

    Add custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    // Fonts
+    fontFamily: {
+      bitter: ['Bitter', 'serif'],
+      macondo: ['Macondo', 'cursive'],
+    },
+    // Colors
+    colors: {
+      primary: '#D80DE2',
+      secondary: '#86DEB7',
+      light: '#FFF8F0',
+      dark: '#050706',
+    },
     extend: {},
   },
   plugins: [],
